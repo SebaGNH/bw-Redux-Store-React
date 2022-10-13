457fec1560971598a93175da096bb0da0318b871
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-10-12 21:12:27 -0300

    se agregó paginador

--- src/components/Pokemones.jsx
+++ src/components/Pokemones.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
 //useSelector lee el array
 import { useDispatch, useSelector } from 'react-redux';
-import {getPokemonsAction} from '../redux/pokeDucks';
+import {getPokemonsAction,nextPokemon} from '../redux/pokeDucks';
 
 const Pokemones = () => {
   //dispatch llama a la acción
   const dispatch = useDispatch();
 
   const pokemones = useSelector((store)=>store.listaPokemones.array)
-  console.log(pokemones)
+  //console.log(pokemones)
 
   return (
-    <>
+    <div className='mt-2'>
 
-      <button onClick={()=> dispatch(getPokemonsAction())}>Get Pokemon</button>
-      
+      <div className="d-flex justify-content-between">
+        <button 
+          className='btn btn-primary'
+          onClick={()=> dispatch(getPokemonsAction())}
+          >Get Pokemon
+        </button> 
+
+        <button 
+          className='btn btn-primary'
+          onClick={()=> dispatch(nextPokemon(20))}
+          >Next Pokemon
+        </button> 
+        <button 
+          className='btn btn-primary'
+          onClick={()=> dispatch(nextPokemon(-20))}
+          >Next Pokemon
+        </button> 
+      </div>
+
+
       <ul>
         {
           pokemones.map(pokemon =>(
@@ -22,7 +40,7 @@ const Pokemones = () => {
           ))
         }
       </ul>
-    </>
+    </div>
   )
 }
 
--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -2,13 +2,15 @@ import axios from 'axios'
 
 // constantes
 const dataInicial = {
-  array : []
+  array : [],
+  offset: 0
 }
 
 
 //types
 //String que se llama igual que la constante
 const getPokemosSuccess = 'getPokemosSuccess';
+const nextPokemosSuccess = 'nextPokemosSuccess';
 
 
 //reducer --las accciones se procesan
@@ -19,6 +21,12 @@ export default function pokeReducer( state = dataInicial, action){
       return {
         ...state,
         array: action.payload  //Mandamos los pokemones al state
+      }
+    case 'nextPokemosSuccess':
+      return {
+        ...state,
+        array: action.payload.array,
+        offset: action.payload.offset
       }
 
       default:
@@ -31,13 +39,36 @@ export default function pokeReducer( state = dataInicial, action){
 
 // acciones
 export const getPokemonsAction = () => async (dispatch,getState) => {
+  const pagina = getState().listaPokemones.offset
   try{
-    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0?limit=20`);
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${pagina}?limit=20`);
     dispatch({
       type: getPokemosSuccess,
       payload: res.data.results 
     });
   }catch(err){
     console.log(err);
+  }
+}
+
+// paginador
+export const nextPokemon = (numero) => async (dispatch,getState) =>{
+
+  const pagina = getState().listaPokemones.offset;
+  if (pagina === 0 && numero === -20) return;
+  const siguiente = pagina + numero;
+
+
+  try {
+    const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${siguiente}?limit=20`);
+    dispatch({
+      type: "nextPokemosSuccess",
+      payload: {
+        array: res.data.results,
+        offset: siguiente
+      }
+    })
+  } catch (error) {
+    console.log(error)
   }
 }
